29123189ee596faaa0fa424f8a1c5da72780dcc0
Paulo <pgsoares@telecomboutique.com>
2021-03-02 12:55:17 +0100

    cambiado ejecutar script por arrancar servicio

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -3,8 +3,10 @@ cd /home/ubuntu/go/src/github.com/nikola43/ecoapi
 
 echo "KILL API"
 sudo pkill ecoapi
+sudo systemctl stop ecoapi
 
 sudo pkill ecoapi
 echo "RUN API"
-./ecoapi &
+#./ecoapi &
+sudo systemctl start ecoapi
 exit
