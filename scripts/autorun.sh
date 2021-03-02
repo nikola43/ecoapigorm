bebbf8ce38f483abf7d5c7a1f424b8cd01b613d2
Paulo <pgsoares@telecomboutique.com>
2021-03-02 12:47:10 +0100

    cerrar script

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 cd /home/ubuntu/go/src/github.com/nikola43/ecoapi
 
-echo "UPDATING REPO"
-git pull
 echo "KILL API"
-
 sudo pkill ecoapi
-echo "BUILD API..."
-
-/usr/local/go/bin/go build
-echo "KILL API"
 
 sudo pkill ecoapi
 echo "RUN API"
 ./ecoapi &
+exit
