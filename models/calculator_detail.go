83012f1a8a4512465bba91f33e507047253facbd
Paulo <pgsoares@telecomboutique.com>
2021-03-02 17:54:00 +0100

    añadido title en promo

--- models/calculator_detail.go
+++ models/calculator_detail.go
@@ -4,7 +4,8 @@ import "gorm.io/gorm"
 
 type CalculatorDetail struct {
 	gorm.Model
-	Week              uint                   `gorm:"type:INTEGER not null" json:"week"`
-	Image              string                   `gorm:"type:varchar(254) not null" json:"image"`
-	Text                 string                 `gorm:"type:varchar(254) not null" json:"text"`
+	Week  uint   `gorm:"type:INTEGER not null" json:"week"`
+	Image string `gorm:"type:varchar(254) not null" json:"image"`
+	Title string `gorm:"type:varchar(64) not null" json:"title"`
+	Text  string `gorm:"type:varchar(254) not null" json:"text"`
 }
