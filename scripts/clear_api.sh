cb4b21cefea6114b0ff07b573e82ae9af50659dc
Paulo <pgsoares@telecomboutique.com>
2021-03-04 13:32:12 +0100

    eliminar ficheros ocultos

--- scripts/clear_api.sh
+++ scripts/clear_api.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 rm -rf /home/ubuntu/go/src/github.com/nikola43/ecoapigorm/*
-rm -rf /home/ubuntu/go/src/github.com/nikola43/ecoapigorm/.*
+rm -rf /home/ubuntu/go/src/github.com/nikola43/ecoapigorm/.env
+rm -rf /home/ubuntu/go/src/github.com/nikola43/ecoapigorm/.git
+rm -rf /home/ubuntu/go/src/github.com/nikola43/ecoapigorm/.gitignore
+rm -rf /home/ubuntu/go/src/github.com/nikola43/ecoapigorm/.idea
 exit
