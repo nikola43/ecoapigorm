cebad6b67395561bf58f4d8df9bb860a3c819494
Miguel Ángel Barrera <mbarrera@plainconcepts.com>
2021-04-03 13:29:43 +0200

    disk space by clinic and client

--- utils/disk_space_util.go
+++ utils/disk_space_util.go
@@ -10,24 +10,24 @@ func CalculateTotalSizeByClient(client models.Client, clinicId uint) uint {
 	totalSize := uint(0)
 
 	// get images size
-	database.GormDB.Table("images").
-		Where("client_id = ?", client.ID).
+	database.GormDB.Model(models.Image{}).
+		Where("client_id = ? AND clinic_id = ?", client.ID, clinicId).
 		Select("IF(size IS NULL, 0, SUM(size)) as size").
 		Scan(&size)
 	totalSize += size
 
 	//get videos size
 	size = 0
-	database.GormDB.Table("videos").
-		Where("client_id = ?", client.ID).
+	database.GormDB.Model(models.Video{}).
+		Where("client_id = ? AND clinic_id = ?", client.ID, clinicId).
 		Select("IF(size IS NULL, 0, SUM(size)) as size").
 		Scan(&size)
 	totalSize += size
 
 	//get heartbeat size
 	size = 0
-	database.GormDB.Table("heartbeats").
-		Where("client_id = ?", client.ID).
+	database.GormDB.Model(models.Heartbeat{}).
+		Where("client_id = ? AND clinic_id = ?", client.ID, clinicId).
 		Select("IF(size IS NULL, 0, SUM(size)) as size").
 		Scan(&size)
 	totalSize += size
