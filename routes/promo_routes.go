12131953b7a7e362b578004a205a1f51d7a9d87a
nikola43 <pauloxti@gmail.com>
2021-07-22 18:18:30 +0200

    ok

--- routes/promo_routes.go
+++ routes/promo_routes.go
@@ -10,17 +10,17 @@ import (
 func PromoRoutes(router fiber.Router) {
 	promoRouter := router.Group("/promo")
 
-	promoRouter.Use(jwtware.New(jwtware.Config{SigningKey: []byte(utils.GetEnvVariable("JWT_EMPLOYEE_KEY"))}))
-
-	// /api/v1/promo/create
-	promoRouter.Post("/", controllers.CreatePromo)
+	// MOBILE ENDPOINT
+	// /api/v1/promo/client
+	promoRouter.Get("/clinic/:clinic_id/client", controllers.GetPromosForClientController)
 
 	// /api/v1/promo/:promo_id
 	promoRouter.Get("/:promo_id", controllers.GetPromoByID)
 
-	// MOBILE ENDPOINT
-	// /api/v1/promo/client
-	promoRouter.Get("/clinic/:clinic_id/client", controllers.GetPromosForClientController)
+	promoRouter.Use(jwtware.New(jwtware.Config{SigningKey: []byte(utils.GetEnvVariable("JWT_EMPLOYEE_KEY"))}))
+
+	// /api/v1/promo/create
+	promoRouter.Post("/", controllers.CreatePromo)
 
 	// /api/v1/promo/:promo_id
 	promoRouter.Delete("/:promo_id", controllers.DeletePromoByID)
